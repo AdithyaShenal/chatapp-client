572086ff98117f435e3c1a8e64ca48c6f44cce6f
AdithyaShenal <washenal55@gmail.com>
2025-07-12 18:28:20 +0530

    socket.io-client URL fixed

--- src/service/socket-client.ts
+++ src/service/socket-client.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://chatapp-server-n84z.onrender.com", {
+  transports: ["websocket"],
+});
 
 export default socket;
